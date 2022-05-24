0213ac48ce7c3121a664d6bbf3009793b89f3a24
Emily Sun <ejs83@duke.edu>
2022-05-24 15:18:12 -0400

    kind of ugly but works -es

--- groups.py
+++ groups.py
@@ -1,7 +1,18 @@
 import random
 
-names = [];
-with open('names.txt') as f:
+group1 = [];
+
+with open('esnames.txt') as f:
     lines = f.readlines();
 
-print(random.sample(lines,3))
+for i in range(len(lines)):
+    lines[i] = lines[i].strip()
+
+
+for i in range(0,3):
+    random_obj = random.choice(lines)
+    lines.remove(random_obj)
+    group1.append(random_obj)
+
+print(group1)
+print(lines)
