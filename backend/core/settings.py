3182cfdb6c08f9d7c3570fc37e057d8ae5600f01
Muaz Bin Kashif <muaz.bin.kashif@duke.edu>
2022-06-07 14:27:18 -0400

    integrated postgresql database to django project

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'core',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -73,10 +74,21 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+   #     'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+     
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django-db', 
+        'USER': 'postgres', 
+        'PASSWORD': 'Cloudcomputing2022',
+        'HOST': '127.0.0.1', 
+        'PORT': '5432',
     }
 }
 
