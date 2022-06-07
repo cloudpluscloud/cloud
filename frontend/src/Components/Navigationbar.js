f6ee9a28c53fb573292843e7e1570a7de302625c
Emily Sun <ejs83@duke.edu>
2022-06-07 15:33:59 -0400

    changed text in navbar

--- frontend/src/Components/Navigationbar.js
+++ frontend/src/Components/Navigationbar.js
@@ -4,20 +4,24 @@ import { Navbar, Nav, NavDropdown, Container } from 'react-bootstrap'
 class Navigationbar extends React.Component {
     render() {
         return (
-            <Navbar bg="light" expand="lg">
+            <Navbar bg="light" expand="lg" sticky="top">
                 <Container>
-                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Brand href="#home">Cloud+</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                            <NavDropdown title="Cloud Platforms" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#cloud/3.1">Microsoft Azure</NavDropdown.Item>
+                                <NavDropdown.Item href="#cloud/3.2">Amazon Web Services</NavDropdown.Item>
+                                <NavDropdown.Item href="#cloud/3.3">Google Cloud Platform</NavDropdown.Item>
                                 <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                                <NavDropdown.Item href="#cloud/3.4">Other</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Services" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#services/3.1">SAAS</NavDropdown.Item>
+                                <NavDropdown.Item href="#services/3.2">PAAS</NavDropdown.Item>
+                                <NavDropdown.Item href="#services/3.3">IAAS</NavDropdown.Item>
                             </NavDropdown>
                         </Nav>
                     </Navbar.Collapse>
