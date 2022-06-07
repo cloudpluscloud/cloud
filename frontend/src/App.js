fefcce20fef3d3c70e1d3222c7fb4ef37c26b1d1
Emily Sun <ejs83@duke.edu>
2022-06-07 15:08:45 -0400

    basic navbar links have not been edited

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import React, { Component } from 'react';
 import './App.css';
+import Navigationbar from "./Components/Navigationbar.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
 
 function App() {
   return (
     <div className="App">
+      <Navigationbar></Navigationbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
