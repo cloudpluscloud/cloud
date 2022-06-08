545e87cf27d4f9191f2f14ac1a8b4ba88eb2a41a
Muaz Bin Kashif <muaz.bin.kashif@duke.edu>
2022-06-08 17:16:50 -0400

    created new service for postgres in compose

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -14,22 +14,27 @@ from pathlib import Path
 import os 
 from dotenv import load_dotenv
 
-load_dotenv()  # take environment variables from .env.
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = Path(__file__).resolve().parent.parent.parent
+
+load_dotenv(os.path.join(ENV_PATH, '.env'))  # take environment variables from .env.
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ayq$3ek4jht^@51p5vlrx2#zt%b!ko22z^tj_+aex2&ag2(==w'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost:3000',
+    'http://127.0.0.1:3000',
+]
 
 
 # Application definition
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tutorials',
 ]
 
 MIDDLEWARE = [
@@ -78,13 +84,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-#DATABASES = {
- #   'default': {
-   #     'ENGINE': 'django.db.backends.sqlite3',
-     #   'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-     
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -92,7 +91,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'), 
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'), 
-        'PORT': '5432',
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
--- backend/tutorials/models.py
+++ backend/tutorials/models.py
@@ -1,6 +1,11 @@
+import uuid
 from django.db import models
-# Create your models here.
-class Teacher(models.Model):
-    name = models.CharField(max_length=80)
-    age = models.IntegerField()
+from django.utils import timezone
 
+# Create your models here.
+class Tutorials(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    title = models.CharField(max_length=200, null=False, unique=True)
+    body = models.CharField(max_length=100000, null=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
