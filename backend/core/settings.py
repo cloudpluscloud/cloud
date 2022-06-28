b0e5b5f9d3a341ad6e4b19c9b4c667cd673008f6
Muaz Bin Kashif <muaz.bin.kashif@duke.edu>
2022-06-28 14:28:40 -0400

    added dependencies

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -88,35 +88,19 @@ WSGI_APPLICATION = "core.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 # [START db_setup]
-if os.getenv('GAE_APPLICATION', None):
-    # Running on production App Engine, so connect to Google Cloud SQL using
-    # the unix socket at /cloudsql/<your-cloudsql-connection string>
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': os.environ.get("DB_HOST"),
-            'USER': os.environ.get("DB_USER"),
-            'PASSWORD': os.environ.get("DB_PASSWORD"),
-            'NAME': os.environ.get("DB_NAME"),
-            'SECRET_KEY': os.environ.get("DB_KEY"),
-        }
+# Running on production App Engine, so connect to Google Cloud SQL using
+# the unix socket at /cloudsql/<your-cloudsql-connection string>
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': os.environ.get("DB_HOST"),
+        'USER': os.environ.get("DB_USER"),
+        'PASSWORD': os.environ.get("DB_PASSWORD"),
+        'NAME': os.environ.get("DB_NAME"),
     }
-else:
-    # Running locally so connect to either a local MySQL instance or connect 
-    # to Cloud SQL via the proxy.  To start the proxy via command line: 
-    #    $ cloud_sql_proxy -instances=[INSTANCE_CONNECTION_NAME]=tcp:3306 
-    # See https://cloud.google.com/sql/docs/mysql-connect-proxy
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'HOST': '127.0.0.1',
-            'PORT': '3306',
-            'NAME': os.environ.get("DB_NAME"),
-            'USER': os.environ.get("DB_USER"),
-            'PASSWORD': os.environ.get("DB_PASSWORD"),
-            'SECRET_KEY': os.environ.get("DB_KEY"),
-        }
-    }
+}
+
 # [END db_setup]
 
 # Password validation
