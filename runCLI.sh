55e3d21c37baf4c5a18eeb3010fa474f564f953d
Emily Sun <emily.j.sun@duke.edu>
2022-07-25 15:50:25 -0400

    more user input for executable

--- runCLI.sh
+++ runCLI.sh
@@ -1,16 +1,27 @@
 az login
 
 templateFile="./personalizedeploy.json"
+parameterFile="./personalizedeploy.parameters.json"
+
+read -p 'Resource Group Name: ' resourceGroupName
 
 echo "creating resource group..."
-az group create --name bashresourcegrouptest --location "eastus"
+az group create \
+--name $resourceGroupName \
+--location "eastus"
 echo "resource group created"
 
-python addresource.py
+python clean.py
+
+python updateparams.py
 
 echo "deploying"
 
-az deployment group create --name usingbash --resource-group bashresourcegrouptest --template-file $templateFile --parameters storageName=bashstoragetest1 
+az deployment group create \
+--name clideploy \
+--resource-group $resourceGroupName \
+--template-file $templateFile \
+--parameters $parameterFile 
 #acrName=acrscripttest2
 
 echo "deployment complete"
