f0d5304baff3936345df04367a11edde2b029f7c
Emily Sun <emily.j.sun@duke.edu>
2022-07-25 15:39:18 -0400

    one liner az commands

--- runCLI.sh
+++ runCLI.sh
@@ -3,20 +3,14 @@ az login
 templateFile="./personalizedeploy.json"
 
 echo "creating resource group..."
-az group create \
---name bashresourcegrouptest \
---location "eastus"
+az group create --name bashresourcegrouptest --location "eastus"
 echo "resource group created"
 
 python addresource.py
 
 echo "deploying"
 
-az deployment group create \
---name usingbash \
---resource-group bashresourcegrouptest \
---template-file $templateFile \
---parameters storageName=bashstoragetest1 
+az deployment group create --name usingbash --resource-group bashresourcegrouptest --template-file $templateFile --parameters storageName=bashstoragetest1 
 #acrName=acrscripttest2
 
 echo "deployment complete"
